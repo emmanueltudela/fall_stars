b30ca5ef874c6523bfc16ac8f300289f2de9fa6e
motzmartin <m4rtinmotz@icloud.com>
2023-01-11 23:53:40 +0100

    Ajout d'un ennemi test.

--- js/enemy.js
+++ js/enemy.js
@@ -3,10 +3,18 @@ class Enemy {
         this.x = x;
         this.y = y;
 
-        this.acceleration_y = 0;
+        this.acceleration_y = 1;
+
+        this.hp = 100;
     }
 
     update() {
         this.y += this.acceleration_y;
+
+        if (this.hp <= 0) {
+            return false;
+        }
+
+        return true;
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+const enemies = [];
+enemies.push(new Enemy(10, 10));
+
 const framerate = 60;
 const interval = 1000 / framerate;
 let now;
@@ -16,7 +19,23 @@ function draw() {
     if (delta > interval) {
         then = now - (delta % interval);
 
+        // Mise a jour des objets
+
+        for (let i = enemies.length - 1; i >= 0; i--) {
+            if (!enemies[i].update()) {
+                enemies.splice(i, 1);
+            }
+        }
+
         // Dessinez ici
+
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "#FFF";
+        for (let i = enemies.length - 1; i >= 0; i--) {
+            ctx.fillRect(enemies[i].x, enemies[i].y, 10, 10);
+        }
     }
 }
 
