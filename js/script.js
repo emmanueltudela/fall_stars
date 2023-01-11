c926c39efabc8e17635a98a56aa833a038f61d78
emmanueltudela <emmanueltudela.pro@gmail.com>
2023-01-11 23:31:11 +0100

    Added classes for testing

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+class Enemies {
+	constructor(x, y, type) {
+
+	}
+}
+
+class Projectiles {
+	constructor() {
+		
+	}
+}
+
+class Crystal {
+	constructor() {
+
+	}
+}
