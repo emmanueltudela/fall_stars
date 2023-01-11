dcc7180f421620b47d6dcdc43b58dde3ab3fd6cd
motzmartin <m4rtinmotz@icloud.com>
2023-01-11 23:29:48 +0100

    Ajout de la fonction draw().

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+const framerate = 60;
+const interval = 1000 / framerate;
+let now;
+let then = Date.now();
+let delta;
+
+function draw()
+{
+    requestAnimationFrame(draw);
+
+    now = Date.now();
+    delta = now - then;
+
+    if (delta > interval)
+    {
+        then = now - (delta % interval);
+
+        // Dessinez ici
+    }
+}
+
+draw();
